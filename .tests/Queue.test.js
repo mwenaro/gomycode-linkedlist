a74c6d829395d88bd729545f78ac398ab10b8ff1
Mwero Abdalla <mwerothewebmaker@gmail.com>
2024-04-18 17:13:30 +0300

    chore : add tests for queue class

---
+++ .tests/Queue.test.js
@@ -0,0 +1,40 @@
+const Queue = require('../Queue');
+
+
+describe('Queue', () => {
+  let stack;
+
+  beforeEach(() => {
+    stack = new Queue();
+  });
+
+  test('isEmpty method', () => {
+    expect(stack.isEmpty()).toBe(true);
+    stack.enqueue(10);
+    expect(stack.isEmpty()).toBe(false);
+  });
+
+  test('enqueue method', () => {
+    stack.enqueue(10, 20, 30);
+    expect(stack.size()).toBe(3);
+  });
+
+  test('peek method', () => {
+    stack.enqueue(10, 20, 80);
+    expect(stack.peek()).toBe(80);
+  });
+
+  test('dequeue method', () => {
+    stack.enqueue(10, 20, 30);
+    expect(stack.dequeue()).toBe(30);
+    expect(stack.size()).toBe(2);
+  });
+
+  test('print method', () => {
+    stack.enqueue(10, 20, 30);
+    const consoleSpy = jest.spyOn(console, 'log').mockImplementation();
+    stack.print();
+    expect(consoleSpy).toHaveBeenCalledWith('10 => 20 => 30');
+    consoleSpy.mockRestore();
+  });
+});
